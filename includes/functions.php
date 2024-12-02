d8f0515f6f2a9306a30ecd1c45bd004f80f05858
Raymond Baghumian <swordart2050@gmail.com>
2024-12-03 02:46:39 +0330

    Move Function contact_handle_form from contactView to functions

--- includes/functions.php
+++ includes/functions.php
@@ -20,3 +20,30 @@ function contact_table() {
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
     dbDelta( $sql );
 }
+
+// Contact Handle Form
+
+function contact_handle_form(){
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    if(empty($name) || empty($email) || empty($message)){
+        wp_die('All Fields are required.');
+    }
+
+    global $wpdb;
+    $tableName = $wpdb->prefix . 'contacts';
+    $wpdb->insert($tableName, [
+        'name' => $name,
+        'email' => $email,
+        'message' => $message,
+        'created_at' => current_time('mysql')
+    ]);
+
+    wp_redirect(home_url('/'));
+    exit;
+}
+add_action( 'admin_post_contact_handle_form', 'contact_handle_form' );
+add_action( 'admin_post_nopriv_contact_handle_form', 'contact_handle_form' );
--- views/contactView.php
+++ views/contactView.php
@@ -31,29 +31,3 @@ function contact_render_form() {
 }
 add_shortcode( 'contact_form', 'contact_render_form' );
 
-// Contact Handle Form
-
-function contact_handle_form(){
-
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-
-    if(empty($name) || empty($email) || empty($message)){
-        wp_die('All Fields are required.');
-    }
-
-    global $wpdb;
-    $tableName = $wpdb->prefix . 'contacts';
-    $wpdb->insert($tableName, [
-        'name' => $name,
-        'email' => $email,
-        'message' => $message,
-        'created_at' => current_time('mysql')
-    ]);
-
-    wp_redirect(home_url('/'));
-    exit;
-}
-add_action( 'admin_post_contact_handle_form', 'contact_handle_form' );
-add_action( 'admin_post_nopriv_contact_handle_form', 'contact_handle_form' );
